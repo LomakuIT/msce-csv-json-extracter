4e373fa36296b7b3165f1cff3da6a9f5a884bac8
MakawaMT <mtmakawa@gmail.com>
2019-06-05 22:14:43 +0200

    Add project files.

---
+++ msce-csv-json-extracter/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace msce_csv_json_extracter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtTransactionLog = new System.Windows.Forms.TextBox();
+            this.dgLog = new System.Windows.Forms.DataGridView();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.selectFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.districtsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schoolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.candidateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.centersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(922, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectFileToolStripMenuItem,
+            this.showLogToolStripMenuItem,
+            this.showFilesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToUseToolStripMenuItem,
+            this.developersToolStripMenuItem,
+            this.contactUsToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // howToUseToolStripMenuItem
+            // 
+            this.howToUseToolStripMenuItem.Name = "howToUseToolStripMenuItem";
+            this.howToUseToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.howToUseToolStripMenuItem.Text = "How To Use";
+            // 
+            // developersToolStripMenuItem
+            // 
+            this.developersToolStripMenuItem.Name = "developersToolStripMenuItem";
+            this.developersToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.developersToolStripMenuItem.Text = "Developers";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFilePath.Location = new System.Drawing.Point(38, 70);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(496, 53);
+            this.lblFilePath.TabIndex = 1;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(596, 81);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(130, 23);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // txtTransactionLog
+            // 
+            this.txtTransactionLog.Location = new System.Drawing.Point(38, 154);
+            this.txtTransactionLog.Multiline = true;
+            this.txtTransactionLog.Name = "txtTransactionLog";
+            this.txtTransactionLog.Size = new System.Drawing.Size(496, 125);
+            this.txtTransactionLog.TabIndex = 3;
+            // 
+            // dgLog
+            // 
+            this.dgLog.AllowUserToAddRows = false;
+            this.dgLog.AllowUserToDeleteRows = false;
+            this.dgLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLog.Location = new System.Drawing.Point(12, 314);
+            this.dgLog.Name = "dgLog";
+            this.dgLog.ReadOnly = true;
+            this.dgLog.RowTemplate.Height = 24;
+            this.dgLog.Size = new System.Drawing.Size(898, 340);
+            this.dgLog.TabIndex = 4;
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(758, 81);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(99, 23);
+            this.btnExtract.TabIndex = 5;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            // 
+            // selectFileToolStripMenuItem
+            // 
+            this.selectFileToolStripMenuItem.Name = "selectFileToolStripMenuItem";
+            this.selectFileToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.selectFileToolStripMenuItem.Text = "Select File";
+            // 
+            // showLogToolStripMenuItem
+            // 
+            this.showLogToolStripMenuItem.Name = "showLogToolStripMenuItem";
+            this.showLogToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.showLogToolStripMenuItem.Text = "Show Log";
+            // 
+            // showFilesToolStripMenuItem
+            // 
+            this.showFilesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.districtsToolStripMenuItem,
+            this.schoolsToolStripMenuItem,
+            this.candidateToolStripMenuItem,
+            this.centersToolStripMenuItem});
+            this.showFilesToolStripMenuItem.Name = "showFilesToolStripMenuItem";
+            this.showFilesToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.showFilesToolStripMenuItem.Text = "Show JSON Files";
+            // 
+            // districtsToolStripMenuItem
+            // 
+            this.districtsToolStripMenuItem.Name = "districtsToolStripMenuItem";
+            this.districtsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.districtsToolStripMenuItem.Text = "Districts";
+            // 
+            // schoolsToolStripMenuItem
+            // 
+            this.schoolsToolStripMenuItem.Name = "schoolsToolStripMenuItem";
+            this.schoolsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.schoolsToolStripMenuItem.Text = "Schools";
+            // 
+            // candidateToolStripMenuItem
+            // 
+            this.candidateToolStripMenuItem.Name = "candidateToolStripMenuItem";
+            this.candidateToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.candidateToolStripMenuItem.Text = "Candidate";
+            // 
+            // centersToolStripMenuItem
+            // 
+            this.centersToolStripMenuItem.Name = "centersToolStripMenuItem";
+            this.centersToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.centersToolStripMenuItem.Text = "Centers";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // contactUsToolStripMenuItem
+            // 
+            this.contactUsToolStripMenuItem.Name = "contactUsToolStripMenuItem";
+            this.contactUsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.contactUsToolStripMenuItem.Text = "Contact Us";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "District Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Overall Log";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "File(s) Path(s)";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 666);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.dgLog);
+            this.Controls.Add(this.txtTransactionLog);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSCE CSV JSON EXTRACTER";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developersToolStripMenuItem;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtTransactionLog;
+        private System.Windows.Forms.DataGridView dgLog;
+        private System.Windows.Forms.ToolStripMenuItem selectFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem districtsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schoolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem candidateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem centersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactUsToolStripMenuItem;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
